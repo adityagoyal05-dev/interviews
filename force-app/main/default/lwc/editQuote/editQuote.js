29e9f2bd8417349a0efb43e35731b07152d2edc1
aditya,goyal@metacube.com <aditya.goyal@metacube.com>
2023-02-23 14:34:52 +0530

    editQuote Updated

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,42 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api , wire} from "lwc";
+import  getQuote from '@salesforce/apex/QuoteController.getQuote';
+import  updateQuote from '@salesforce/apex/QuoteController.updateQuote';
 
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
-  };
+  quoteData = [];
+
+
+  @wire(getQuote, {recordDataId : '$recordId'})
+  getQuoteData(result) {
+      debugger;
+      if (result.error) {
+          this.errorMessage = JSON.stringify(error);
+      } else if (result.data !== undefined) {
+               this.quoteData = result.data;
+      }
+  };  
+
+  saveUpdatedQuote(){
+    debugger;
+    if(updateQuote.length > 0){
+        updateQuote({dataJSON: JSON.stringify(this.quoteData)})
+        .then(data=>{
+            if(data){
+                this.showMessage('Record(s) are saved succesfully!', 'Success', 'success', 'dismissable');
+            }else{
+                this.showMessage('Record(s) are not saved!', 'Error', 'error', 'dismissable');
+            }
+        }).catch(error=>{
+            this.showMessage('System Error: '+JSON.stringify(error.body.message), 'Error', 'error', 'dismissable');
+        })
+
+    }
+}
+
 
   renderedCallback() {}
 }
